e7434b6c059072f531eedbc7e5e62a079101135e
adrinorosario <rosarioadrino@gmail.com>
2026-01-18 16:38:29 +0530

    set up the navigation from home page to add pin, navigation back to homepage after adding the pin details yet to be implemented; alignment sorted for now in the add pin page and all widgets properly containerised to improve better scaling and further UI enhancements; comments not yet added, need to add them

--- README.md
+++ README.md
@@ -1,16 +1,38 @@
-# Wildlife Animal Tracking App (in development)
+Wildlife helpline and conservation platform
 
-A new Flutter project.
+Features:
 
-## Getting Started
+    •	AI recognition of injured/lost animals- Rangers, tourists, locals/anybody who encounters an injured/lost animal in the woods/urban areas can click a picture of them and send an alert to the nearby vet/officers who can send help.
+    •	Emergency SOS- Activate a siren like broadcast to all nearby clinics, rangers, officers for quicker help.
+    •	GPS tracking- The user selects the current location of where they are reporting the incident from. Maps used to signal the others
+    •	Offline maps, alerts, SOS features
+    •	Categories of alert:
+    •	 Injury: 2 days timeframe- RED
+    •	Sighting: 6 hours- BLUE
+    •	Lost in urban areas: 1 day- YELLOW
+    •	Displaced or isolated: 12 hours- ORANGE- emits radius
 
-This project is a starting point for a Flutter application.
+Users:
+• Tourists, Locals, Urban public
+• Rangers, Doctors, vets, Forest officers
 
-A few resources to get you started if this is your first Flutter project:
+Functionality:
+• Automatic location mapping: When you launch the app, it traces your current location and filters out the data required by the AI model.
+• Camera: Take picture, model recognises the location, species etc. and sends the alert. If online, image gets uploaded immediately. If offline, only the credentials are broadcasted and the image is uploaded once online.
+• Alert pin: Will contain name, location, type of emergency and will redirect to google map
+• Catalog with all pins uploaded with status, date, type, images etc- available online: This page appears after reporting is done, this is not the primary home page.
+• Map: The pins of the current location will be highlighted when zoomed in. As we zoom out, only the most spotted animals will appear.
+• AI model: Recognises the animal, fills out needed information in the details page.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Pages:
+• Map with pins, recently updated.
+• Camera page that slides/ pops up
+• Details page with categories etc and then submit- map gets updated
+• Back to default page-map
+• Emergency notifications
+• Catalog
+• Settings
+• Admin Login in settings:
+• Ranger credentials-login
+• Emergency siren only for admin- extreme injury case
+• Acknowledgment of pin-only for admin: Rescued/Treated
--- lib/add_pin.dart
+++ lib/add_pin.dart
@@ -52,7 +52,7 @@ class _AddPinState extends State<AddPin> {
     return Scaffold(
       appBar: AppBar(title: const Text("Add Pin")),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(30.0),
         child: SingleChildScrollView(
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.center,
@@ -62,84 +62,93 @@ class _AddPinState extends State<AddPin> {
                   children: [
                     SizedBox(height: 250, child: CameraCapture()),
                     const SizedBox(height: 30),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            const Text(
-                              "Status:",
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            DropdownMenu<PinType>(
-                              initialSelection: PinType.values.first,
-                              onSelected: (PinType? value) {
-                                setState(() {
-                                  dropDownValue = value!.title;
-                                });
-                              },
-                              dropdownMenuEntries: pinTypeEntries,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 40),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            const Text(
-                              "Siren:",
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            // ToggleSwitch(
-                            //   minWidth: 90.0,
-                            //   cornerRadius: 20.0,
-                            //   activeBgColors: [
-                            //     [Colors.green[800]!],
-                            //     [Colors.red[800]!],
-                            //   ],
-                            //   activeFgColor: Colors.white,
-                            //   inactiveBgColor: Colors.grey,
-                            //   inactiveFgColor: Colors.white,
-                            //   initialLabelIndex: 1,
-                            //   totalSwitches: 2,
-                            //   labels: ['Active', 'Inactive'],
-                            //   radiusStyle: true,
-                            //   onToggle: (index) {
-                            //     setState(() {
-                            //       isSirenActive = !isSirenActive;
-                            //     });
-                            //   },
-                            // ),
-                            SegmentedButton<bool>(
-                              style: SegmentedButton.styleFrom(
-                                backgroundColor: Colors.grey[200],
-                                foregroundColor: Colors.black,
-                                selectedBackgroundColor: isSirenActive
-                                    ? Colors.green
-                                    : Colors.red,
-                                selectedForegroundColor: Colors.white,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              const SizedBox(
+                                width: 100,
+                                child: Text(
+                                  "Status:",
+                                  style: TextStyle(fontSize: 16),
+                                ),
                               ),
-                              segments: [
-                                ButtonSegment(
-                                  value: true,
-                                  label: Text("Active"),
+                              DropdownMenu<PinType>(
+                                initialSelection: PinType.values.first,
+                                onSelected: (PinType? value) {
+                                  setState(() {
+                                    dropDownValue = value!.title;
+                                  });
+                                },
+                                dropdownMenuEntries: pinTypeEntries,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 40),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              const SizedBox(
+                                width: 100,
+                                child: Text(
+                                  "Siren:",
+                                  style: TextStyle(fontSize: 16),
                                 ),
-                                ButtonSegment(
-                                  value: false,
-                                  label: Text("Inactive"),
+                              ),
+                              // ToggleSwitch(
+                              //   minWidth: 90.0,
+                              //   cornerRadius: 20.0,
+                              //   activeBgColors: [
+                              //     [Colors.green[800]!],
+                              //     [Colors.red[800]!],
+                              //   ],
+                              //   activeFgColor: Colors.white,
+                              //   inactiveBgColor: Colors.grey,
+                              //   inactiveFgColor: Colors.white,
+                              //   initialLabelIndex: 1,
+                              //   totalSwitches: 2,
+                              //   labels: ['Active', 'Inactive'],
+                              //   radiusStyle: true,
+                              //   onToggle: (index) {
+                              //     setState(() {
+                              //       isSirenActive = !isSirenActive;
+                              //     });
+                              //   },
+                              // ),
+                              SegmentedButton<bool>(
+                                style: SegmentedButton.styleFrom(
+                                  backgroundColor: Colors.grey[200],
+                                  foregroundColor: Colors.black,
+                                  selectedBackgroundColor: isSirenActive
+                                      ? Colors.green
+                                      : Colors.red,
+                                  selectedForegroundColor: Colors.white,
                                 ),
-                              ],
-                              selected: <bool>{isSirenActive},
-                              onSelectionChanged: (Set<bool> newSelection) {
-                                setState(() {
-                                  isSirenActive = newSelection.first;
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
+                                segments: [
+                                  ButtonSegment(
+                                    value: true,
+                                    label: Text("Active"),
+                                  ),
+                                  ButtonSegment(
+                                    value: false,
+                                    label: Text("Inactive"),
+                                  ),
+                                ],
+                                selected: <bool>{isSirenActive},
+                                onSelectionChanged: (Set<bool> newSelection) {
+                                  setState(() {
+                                    isSirenActive = newSelection.first;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -148,7 +157,32 @@ class _AddPinState extends State<AddPin> {
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text(
+                          "Pin submitted",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        duration: const Duration(seconds: 2),
+                        width: 300,
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        behavior: SnackBarBehavior.floating,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Colors.green[500],
+                        action: SnackBarAction(
+                          label: "Dismiss",
+                          textColor: Colors.white,
+                          onPressed: () {},
+                        ),
+                      ),
+                    );
+                  },
                   child: const Text("Submit Pin"),
                   style: ElevatedButton.styleFrom(
                     minimumSize: const Size(200, 50),
