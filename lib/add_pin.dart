a26f616135ff3a5ef1e21e4ab9f800e9e93817de
Adrino Rosario <rosarioadrino@gmail.com>
2026-02-19 19:57:43 +0530

    added new ui

--- lib/add_pin.dart
+++ lib/add_pin.dart
@@ -7,18 +7,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-class SavannahColors {
-  static const Color beigeLight = Color(0xFFF6F1E1);
-  static const Color beigeDark = Color(0xFFECE6D4);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color greenOlive = Color(0xFF4F5D2F);
-  static const Color greenDeep = Color(0xFF3E4A24);
-  static const Color orangeCaramel = Color(0xFFC88A3D);
-  static const Color orangeSand = Color(0xFFE3B071);
-  static const Color textBlack = Color(0xFF1F1F1F);
-  static const Color textGrey = Color(0xFF4B4B4B);
-}
+import 'package:wildlife_tracker/theme_colors.dart';
 
 enum PinType {
   injured(
